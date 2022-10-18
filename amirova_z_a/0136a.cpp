676252fd34876cfe19c976451d7beabe8db047fc
Zarina-Amirova <113860256+Zarina-Amirova@users.noreply.github.com>
2022-10-18 09:00:10 +0300

    Update 0136a.cpp

--- amirova_z_a/0136a.cpp
+++ amirova_z_a/0136a.cpp
@@ -1,16 +1,16 @@
 #include <iostream>
 #include <vector>
-
+ 
 int main()
 {
 	int n, p = 0;
 	std::cin >> n;
-	std::vector<int> p(n);
+	std::vector<int> ps(n);
 	for (int i = 0; i < n; i += 1) {
 		std::cin >> p;
-		p[p - 1] = i + 1;
+		ps[p - 1] = i + 1;
 	}
 	for (int j = 0; j < n; j += 1) {
-		std::cout << p[j] << std::endl;
+		std::cout << ps[j] << std::endl;
 	}
 }
