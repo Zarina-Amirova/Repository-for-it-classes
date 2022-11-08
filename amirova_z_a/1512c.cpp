2fd69e233fdbc5e9b57a0b0d429c89f6fdbd4e3b
Zarina-Amirova <113860256+Zarina-Amirova@users.noreply.github.com>
2022-11-08 12:07:18 +0300

    Update 1512c.cpp

--- amirova_z_a/1512c.cpp
+++ amirova_z_a/1512c.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+	int t, a, b, s;
+	std::cin >> t;
+	for (int i = 0; i < t; i += 1) {
+		std::cin >> a >> b;
+		s = a + b;
+		std::vector<int> pal(s);
+		for (int j = 0; j < t; j += 1) {
+			std::cin >> pal[j];
+		}
+	}
+}
