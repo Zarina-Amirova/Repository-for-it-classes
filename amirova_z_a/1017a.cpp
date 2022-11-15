a2031c79075d0d4229c5c22ed93519091fda2192
Zarina-Amirova <113860256+Zarina-Amirova@users.noreply.github.com>
2022-11-15 11:30:58 +0300

    Update 1017a.cpp

--- amirova_z_a/1017a.cpp
+++ amirova_z_a/1017a.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+ 
+int main()
+{
+	int n;
+	std::cin >> n;
+	int x1, x2, x3, x4;
+	std::cin >> x1 >> x2 >> x3 >> x4;
+	int s_std = x1 + x2 + x3 + x4;
+	int ans = 1;
+	for (int i = 1; i < n; i += 1) {
+		std::cin >> x1 >> x2 >> x3 >> x4;
+		if (x1 + x2 + x3 + x4 > s_std) {
+			ans += 1;
+		}
+	}
+	std::cout << ans;
+ 
+}
